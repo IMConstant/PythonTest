79564a777e79864ff86b42ec920153d4eb296e44
SpceForMind <cahsa_davydov@mail.ru>
2018-04-04 14:19:58 +0300

    ch2

--- Files/TestCreator.py
+++ Files/TestCreator.py
@@ -10,50 +10,52 @@ start = os.getcwd()
 dirs = ['add', 'mul']
 num = 0
 
-def Creator(deway):
+def Creator(deway, file_count):
 	rnddf = random.randint(0, 10)
 
 	if rnddf <= rndflag:
 		CreateDir(deway)
-		CreateNext(deway)
+		file_count = CreateNext(deway, file_count)
 
-	CreateFiles(deway)
+	return CreateFiles(deway, file_count)
 
-def CreateFiles(deway):
-	global num #txt files count
-
+def CreateFiles(deway, file_count):
 	for j in range(1, random.randint(2, 10)):
-		fout = open(os.path.join(deway, 'file' + str(num) + '.txt'), 'w')
+		fout = open(os.path.join(deway, 'file' + str(file_count) + '.txt'), 'w')
 
 		for i in range(5):
 			fout.write(str(random.randint(1, max_num)))
 			fout.write(' ')
 
 		fout.close()
-		num += 1
+		file_count += 1
+
+	return file_count
 
 
-def CreateNext(deway):
+def CreateNext(deway, file_count):
 	for file in os.listdir(deway):
 		if os.path.isdir(os.path.join(deway, file)):
-			Creator(os.path.join(deway, file))
+			file_count = Creator(os.path.join(deway, file), file_count)
 
-def CreateDir(deway, flg = 0):
-	if flg == 0:
+	return file_count
+
+def CreateDir(deway, flg = False):
+	if not flg:
 		if random.randint(1, 2) == 2:
-			os.mkdir(os.path.join(deway, 'add'))
-			os.mkdir(os.path.join(deway, 'mul'))
+			for file in dirs:
+				os.mkdir(os.path.join(deway, file))
 
 			return
 
 	os.mkdir(os.path.join(deway, str(random.choice(dirs))))
 
-def CreateStartDirs():
+def CreateStartDirs(file_count):
 	for i in range(1, amount + 1):
         	way = os.path.join(start, 'test' + (('0' + str(i)) if i < 10 else str(i)))
         	os.mkdir(way)
         	CreateDir(way, True)
-        	CreateNext(way)
+        	file_count = CreateNext(way, file_count)
 
 def CheckProcIn():
 	parser = argparse.ArgumentParser()
@@ -83,4 +85,4 @@ random.seed()
 
 DeleteTests()
 max_num, rndflag, amount = CheckProcIn()
-CreateStartDirs()
+CreateStartDirs(num)
